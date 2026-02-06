4b7d734be169b38610c9088f048ae420136bbc0b
johnny60904 <privateuser60904@gmail.com>
2026-02-07 00:58:23 +0800

    feat(Public-Cmdlet): add pipeline input support for path-based invocation  / support pipeline input by value and property name
    Signed-off-by: johnny60904 <privateuser60904@gmail.com>

--- src/Public/Cmdlets/Get-Files.ps1
+++ src/Public/Cmdlets/Get-Files.ps1
@@ -1,9 +1,12 @@
 
 function Get-Files {
 # TODO: Add Comment-Based Help
-    [CmdletBinding()]
+    [CmdletBinding(DefaultParameterSetName = 'ByValue')]
     param(
-        [Parameter(Mandatory, Position = 0)]
+        
+        # ------------------ 核心參數 ------------------
+        
+        [Parameter(Mandatory, Position = 0, ParameterSetName = 'ByValue')]
         [ValidateScript({
             [IOPathSyntaxValidators]::ValidateAbsolutePathSyntax($_, 'Path')
             [FileSystemStateValidators]::ValidateDirectoryExistence($_, 'Path')
@@ -11,8 +14,19 @@ function Get-Files {
         })]
         [string] $Path, # DirectoryPath
         
+        [Parameter(Mandatory, ValueFromPipeline, ValueFromPipelineByPropertyName, ParameterSetName = 'ByPipeline')]
+        [ValidateScript({
+            [IOPathSyntaxValidators]::ValidateAbsolutePathSyntax($_, 'InputPath')
+            [FileSystemStateValidators]::ValidateDirectoryExistence($_, 'InputPath')
+            return $true
+        })]
+        [string] $InputPath, # # DirectoryPath
+        
+        # ------------------ 共用參數 ------------------
+        
         # 可空 (不指定), 就變成遍歷並收集 $Path 下所有檔案
-        [Parameter(Position = 1)]
+        [Parameter(Position = 1, ParameterSetName = 'ByValue')]
+        [Parameter(Position = 0, ParameterSetName = 'ByPipeline')]
         [ValidateScript({
             if ([IOPathSegmentValidators]::ValidateIsNullOrEmpty($_)) { return $true }
             [IOPathSegmentValidators]::ValidateFolderNameSyntax($_, 'FilterNames')
@@ -21,28 +35,21 @@ function Get-Files {
         [string[]] $FilterNames = @(), # ChildNames
         
         # 因為是 Files 用, 所以要求有附檔名, 即格式: xxx.xxx / 特例允許 *.* / *.<xxx> / <xxx>.*
-        [Parameter(Position = 2)]
+        [Parameter(Position = 2, ParameterSetName = 'ByValue')]
+        [Parameter(Position = 1, ParameterSetName = 'ByPipeline')]
         [ValidateScript({
             [FileFilterPatternValidators]::ValidateFileFilterPattern($_, 'Filter')
             return $true
         })]
         [string] $Filter = "*.*", # FileFilter
         
-        # 基本上在 New-Files*Options 那邊都 Validate 完了, 所以這邊應該就不處理了
-        # 頂多使用者亂傳 hashtable / pscustomobject 之類的, 無法轉換成 FilesOptions,
-        # 但也會 powershell 原生自動噴錯, 等於第一層防護了
-        
-        [Parameter(Position = 3)]
-        [FilesOptions] $Options = (New-FilesOptions),
-        
-        [Parameter(Position = 4)]
-        [FilesAdvancedOptions] $Advanced = (New-FilesAdvancedOptions),
-        
         [Parameter()]
         [switch] $Recurse,
         
         [Parameter()]
         [switch] $DepthFirst,
+        
+        # ------------------ Default switches ------------------
         
         [Parameter()]
         [switch] $ExcludeHidden,
@@ -51,63 +58,101 @@ function Get-Files {
         [switch] $ExcludeSystem,
         
         [Parameter()]
-        [switch] $ExcludeReadonly
+        [switch] $ExcludeReadonly,
+        
+        # ------------------ Options 相合組合 / 不互斥 ------------------
+        # ------------------------------------------------
+        # 基本上在 New-Files*Options 那邊都 Validate 完了, 所以這邊應該就不處理了
+        # 頂多使用者亂傳 hashtable / pscustomobject 之類的, 無法轉換成 FilesOptions,
+        # 但也會 powershell 原生自動噴錯, 等於第一層防護了
+        # ------------------------------------------------
+        
+        [Parameter(Position = 3, ParameterSetName = 'ByValue')]
+        [Parameter(Position = 2, ParameterSetName = 'ByPipeline')]
+        [FilesOptions] $Options = (New-FilesOptions),
+        
+        [Parameter(Position = 4, ParameterSetName = 'ByValue')]
+        [Parameter(Position = 3, ParameterSetName = 'ByPipeline')]
+        [FilesAdvancedOptions] $Advanced = (New-FilesAdvancedOptions)
         
     )
-    [bool]$RecurseFlag = if ($Recurse.IsPresent) { $true } else { $false }
-    [bool]$DepthFirstFlag = if ($DepthFirst.IsPresent) { $true } else { $false }
-    [bool]$ExcludeHiddenFlag = if ($ExcludeHidden.IsPresent) { $true } else { $false }
-    [bool]$ExcludeSystemFlag = if ($ExcludeSystem.IsPresent) { $true } else { $false }
-    [bool]$ExcludeReadonlyFlag = if ($ExcludeReadonly.IsPresent) { $true } else { $false }
-    try {
-        [TraversalOptions]$traversalOptions = [DiscoveryOptionsMapper]::Map(
-            $Options.IgnoreErrors,
-            $Options.CaseSensitivity,
-            $Advanced.BufferSizeKB,
-            $ExcludeHiddenFlag,
-            $ExcludeSystemFlag,
-            $ExcludeReadonlyFlag,
-            $Advanced.ExcludeAttributes
-        )
-    } catch [ApplicationException] {
-        [System.Management.Automation.ErrorRecord]$err = [ErrorRecordFactory]::FromException(
-            $_.Exception,
-            'UseCaseComponentFailed',
-            [System.Management.Automation.ErrorCategory]::InvalidOperation,
-            $null
-        )
-        $PSCmdlet.ThrowTerminatingError($err)
+    begin {
+        [bool]$RecurseFlag = if ($Recurse.IsPresent) { $true } else { $false }
+        [bool]$DepthFirstFlag = if ($DepthFirst.IsPresent) { $true } else { $false }
+        [bool]$ExcludeHiddenFlag = if ($ExcludeHidden.IsPresent) { $true } else { $false }
+        [bool]$ExcludeSystemFlag = if ($ExcludeSystem.IsPresent) { $true } else { $false }
+        [bool]$ExcludeReadonlyFlag = if ($ExcludeReadonly.IsPresent) { $true } else { $false }
     }
-    try {
-        [DiscoveryRequest]$request = [DiscoveryRequestMapper]::Map(
-            $Path,
-            $FilterNames,
-            $Filter,
-            $traversalOptions,
-            $RecurseFlag,
-            $DepthFirstFlag
-        )
-    } catch [ApplicationException] {
-        [System.Management.Automation.ErrorRecord]$err = [ErrorRecordFactory]::FromException(
-            $_.Exception,
-            'UseCaseComponentFailed',
-            [System.Management.Automation.ErrorCategory]::InvalidOperation,
-            $null
-        )
-        $PSCmdlet.ThrowTerminatingError($err)
+    process {
+        [string]$directory = $null
+        switch ($PSCmdlet.ParameterSetName) {
+            'ByValue' { [string]$directory = $Path }
+            'ByPipeline' { [string]$directory = $InputPath }
+            default {
+                [System.InvalidOperationException]$excp = [System.InvalidOperationException]::new(
+                    "Unknown parameter set in Get-Files."
+                )
+                [System.Management.Automation.ErrorRecord]$err = [ErrorRecordFactory]::FromException(
+                    $excp,
+                    'UnsupportedParameterSet',
+                    [System.Management.Automation.ErrorCategory]::InvalidOperation,
+                    $null
+                )
+                $PSCmdlet.ThrowTerminatingError($err)
+            }
+        }
+        try {
+            [TraversalOptions]$traversalOptions = [DiscoveryOptionsMapper]::Map(
+                $Options.IgnoreErrors,
+                $Options.CaseSensitivity,
+                $Advanced.BufferSizeKB,
+                $ExcludeHiddenFlag,
+                $ExcludeSystemFlag,
+                $ExcludeReadonlyFlag,
+                $Advanced.ExcludeAttributes
+            )
+        } catch [ApplicationException] {
+            [System.Management.Automation.ErrorRecord]$err = [ErrorRecordFactory]::FromException(
+                $_.Exception,
+                'UseCaseComponentFailed',
+                [System.Management.Automation.ErrorCategory]::InvalidOperation,
+                $null
+            )
+            $PSCmdlet.ThrowTerminatingError($err)
+        }
+        try {
+            [DiscoveryRequest]$request = [DiscoveryRequestMapper]::Map(
+                $directory,
+                $FilterNames,
+                $Filter,
+                $traversalOptions,
+                $RecurseFlag,
+                $DepthFirstFlag
+            )
+        } catch [ApplicationException] {
+            [System.Management.Automation.ErrorRecord]$err = [ErrorRecordFactory]::FromException(
+                $_.Exception,
+                'UseCaseComponentFailed',
+                [System.Management.Automation.ErrorCategory]::InvalidOperation,
+                $null
+            )
+            $PSCmdlet.ThrowTerminatingError($err)
+        }
+        [DiscoverStructuredFilesSet]$useCase = [DiscoverStructuredFilesSet]::new($request)
+        try {
+            [string[]]$discovered = ($useCase.Execute()).DiscoveredFiles
+        } catch [ApplicationException] {
+            [System.Management.Automation.ErrorRecord]$err = [ErrorRecordFactory]::FromException(
+                $_.Exception,
+                'UseCaseFailed',
+                [System.Management.Automation.ErrorCategory]::InvalidOperation,
+                $null
+            )
+            $PSCmdlet.ThrowTerminatingError($err)
+        }
     }
-    [DiscoverStructuredFilesSet]$useCase = [DiscoverStructuredFilesSet]::new($request)
-    try {
-        [string[]]$discovered = ($useCase.Execute()).DiscoveredFiles
+    end {
         return [string[]]$discovered
-    } catch [ApplicationException] {
-        [System.Management.Automation.ErrorRecord]$err = [ErrorRecordFactory]::FromException(
-            $_.Exception,
-            'UseCaseFailed',
-            [System.Management.Automation.ErrorCategory]::InvalidOperation,
-            $null
-        )
-        $PSCmdlet.ThrowTerminatingError($err)
     }
 }
 # (.SYNOPSIS)
